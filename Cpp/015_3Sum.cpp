caaa77a10648662479563269d444bbc17e5e59ed
Sebastien Blanchet <s3blanch@edu.uwaterloo.ca>
2018-06-06 11:59:40 -0700

    Initial attempt at #015 3Sum py

---
+++ Cpp/015_3Sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+
+}
+
+
+// Driver
+int main(){
+
+  // init vector
+  vector<int> nums;
+  nums.push_back();
+
+  // vector of vectors?
+  vector<vector<int>> ans = threeSum(nums);
+
+  return 0;
+}
